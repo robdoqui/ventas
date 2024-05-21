6ef73b72e7b16c55ad4585704f87bbca20695027
robdoqui <rob_jimenez@hotmail.com>
2024-05-21 11:33:29 -0400

    Update products.php

--- products.php
+++ products.php
@@ -3,9 +3,9 @@ $products = [
     ["id" => 1, "name" => "Producto 1", "price" => 10.0],
     ["id" => 2, "name" => "Producto 2", "price" => 20.0],
     ["id" => 3, "name" => "Producto 3", "price" => 30.0],
-    ["id" => 3, "name" => "Producto 4", "price" => 40.0],
-    ["id" => 3, "name" => "Producto 5", "price" => 50.0],
-    ["id" => 3, "name" => "Producto 6", "price" => 60.0],
+    ["id" => 4, "name" => "Producto 4", "price" => 40.0],
+    ["id" => 5, "name" => "Producto 5", "price" => 50.0],
+    ["id" => 6, "name" => "Producto 6", "price" => 60.0],
 ];
 
 if (isset($_GET['id'])) {
